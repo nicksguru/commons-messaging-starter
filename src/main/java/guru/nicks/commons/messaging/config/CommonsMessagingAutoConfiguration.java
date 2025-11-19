d74a81a2f41236afb07fb97b6f05eb4885503e26
Nick S. <job-offers@nicks.guru>
2025-11-19 15:22:36 +0300

    Bumped version number

--- src/main/java/guru/nicks/commons/messaging/config/CommonsMessagingAutoConfiguration.java
+++ src/main/java/guru/nicks/commons/messaging/config/CommonsMessagingAutoConfiguration.java
@@ -5,18 +5,24 @@ import guru.nicks.commons.messaging.resolver.MessageTypeResolver;
 import guru.nicks.commons.messaging.service.MessagePublisherService;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
+@Slf4j
 public class CommonsMessagingAutoConfiguration {
 
+    /**
+     * Creates {@link MessagePublisherService} bean if it's not already present.
+     */
     @ConditionalOnMissingBean(MessagePublisherService.class)
     @Bean
     public MessagePublisherService messagePublisherService(StreamBridge streamBridge,
             MessageTypeResolver messageTypeResolver, ObjectMapper objectMapper) {
+        log.debug("Building {} bean", MessagePublisherService.class.getSimpleName());
         return new KafkaMessagePublisherServiceImpl(streamBridge, messageTypeResolver, objectMapper);
     }
 
