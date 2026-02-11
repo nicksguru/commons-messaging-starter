ad8de3b7212c6f1eae8add6f7c96549c80eeb862
Nick S. <job-offers@nicks.guru>
2026-02-11 17:39:20 +0300

    fix: add autoconfig manifest

--- src/main/java/guru/nicks/commons/messaging/config/CommonsMessagingAutoConfiguration.java
+++ src/main/java/guru/nicks/commons/messaging/config/CommonsMessagingAutoConfiguration.java
@@ -1,10 +1,9 @@
 package guru.nicks.commons.messaging.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.nicks.commons.messaging.impl.KafkaMessagePublisherServiceImpl;
 import guru.nicks.commons.messaging.resolver.MessageTypeResolver;
 import guru.nicks.commons.messaging.service.MessagePublisherService;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.stream.function.StreamBridge;
